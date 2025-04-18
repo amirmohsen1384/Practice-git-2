26548dc64577ff005bc7d63444f4ff5eb51e9cfb
Amir Mohsen Ghasemi <78074373+amirmohsen1384@users.noreply.github.com>
2025-04-18 09:59:23 +0330

    Second Commit

--- Sample.cpp
+++ Sample.cpp
@@ -1,8 +1,13 @@
 #include <iostream>
 using namespace std;
 
+void PrintStudentNumber() {
+    cout << "Student Number: 4033613066" << endl;
+}
+
 // This is a sample C++ program
 int main() {
-    cout << "Hello, world.";
+    cout << "Hello, world." << '\n';
+    PrintStudentNumber();
     return 0;
 }
