864deeae6612686113f785ffe4305e92eee558bc
Amir Mohsen Ghasemi <78074373+amirmohsen1384@users.noreply.github.com>
2025-04-18 10:02:46 +0330

    Revert"Second Commit"
    This reverts commit 26548dc64577ff005bc7d63444f4ff5eb51e9cfb.

--- Sample.cpp
+++ Sample.cpp
@@ -1,13 +1,8 @@
 #include <iostream>
 using namespace std;
 
-void PrintStudentNumber() {
-    cout << "Student Number: 4033613066" << endl;
-}
-
 // This is a sample C++ program
 int main() {
-    cout << "Hello, world." << '\n';
-    PrintStudentNumber();
+    cout << "Hello, world.";
     return 0;
 }
